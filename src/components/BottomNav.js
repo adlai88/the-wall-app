550b07bcd42c9b9d09c022f96afa45f054416d11
adlai88 <adlai88@gmail.com>
2025-05-04 23:39:49 +0800

    Update BottomNav.js

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import { FiMap, FiNavigation, FiList, FiInfo } from 'react-icons/fi';
 
 const NavContainer = styled.div`
-  height: 60px;
+  height: 90px;
   background-color: white;
   border-top: 1px solid #222;
   display: flex;
@@ -17,116 +17,8 @@ const NavContainer = styled.div`
   z-index: 1000;
   /* Remove isolation property */
   /* Add padding for iOS safe area and extra comfort */
-  padding-bottom: calc(env(safe-area-inset-bottom, 0px) + 36px);
-  padding-top: 36px;
+  padding-bottom: calc(env(safe-area-inset-bottom, 0px) + 24px);
+  padding-top: 16px;
 `;
 
 const NavItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: inherit;
-  font-size: 14px;
-  font-weight: 500;
-  color: ${props => props.$active ? 'black' : '#666'};
-  cursor: pointer;
-  letter-spacing: 0;
-  flex: 1;
-  max-width: 25%;
-`;
-
-const NavIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  /* background-color: ${props => props.$active ? 'black' : '#ddd'}; */
-  margin-bottom: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${props => props.$active ? 'black' : '#222'};
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const LocationButton = styled(NavItem)`
-  color: ${props => props.$isLocating ? '#999' : '#666'};
-  cursor: ${props => props.$isLocating ? 'wait' : 'pointer'};
-`;
-
-const AddPosterButton = styled.button`
-  display: none;
-  @media (max-width: 768px) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 50px;
-    height: 50px;
-    background-color: black;
-    color: white;
-    font-size: 30px;
-    font-weight: bold;
-    border: 1px solid #222;
-    cursor: pointer;
-    margin: 0 10px;
-    flex-shrink: 0;
-    
-    &:hover {
-      background-color: white;
-      color: black;
-    }
-  }
-`;
-
-function BottomNav({ active, onLocationClick, isLocating, onNav, onAddPoster, isPlacingPin }) {
-  const router = useRouter();
-  
-  return (
-    <NavContainer>
-      <NavItem 
-        $active={active === 'map'} 
-        onClick={() => onNav ? onNav('map') : router.push('/')}
-      >
-        <NavIcon $active={active === 'map'}><FiMap size={18} /></NavIcon>
-        <span>Map</span>
-      </NavItem>
-      <LocationButton
-        $isLocating={isLocating}
-        onClick={(e) => {
-          console.log('LocationButton clicked', onLocationClick);
-          if (typeof onLocationClick === 'function') {
-            onLocationClick(e);
-          }
-        }}
-        $active={false}
-        aria-label="Current Location"
-        tabIndex={0}
-        role="button"
-        style={{ outline: 'none' }}
-      >
-        <NavIcon $active={false}>
-          <FiNavigation />
-        </NavIcon>
-        <span>Location</span>
-      </LocationButton>
-      <AddPosterButton onClick={onAddPoster} aria-label={isPlacingPin ? "Exit Add Poster" : "Add Poster"}>
-        {isPlacingPin ? '×' : '+'}
-      </AddPosterButton>
-      <NavItem 
-        $active={active === 'upcoming'} 
-        onClick={() => onNav ? onNav('upcoming') : router.push('/upcoming')}
-      >
-        <NavIcon $active={active === 'upcoming'}><FiList size={18} /></NavIcon>
-        <span>List</span>
-      </NavItem>
-      <NavItem 
-        $active={active === 'about'} 
-        onClick={() => onNav ? onNav('about') : router.push('/about')}
-      >
-        <NavIcon $active={active === 'about'}><FiInfo size={18} /></NavIcon>
-        <span>About</span>
-      </NavItem>
-    </NavContainer>
-  );
-}
-
-export default BottomNav;
