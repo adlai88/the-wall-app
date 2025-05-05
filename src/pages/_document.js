d03cd5c529c9ae26aa514ef7827451a4c4325297
adlai88 <adlai88@gmail.com>
2025-05-06 03:32:09 +0800

    update metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,9 +10,10 @@ export default function Document() {
         <link rel="icon" href="/icons/icon-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Wallllll Map" />
-        <meta name="description" content="A community wall for events and posters." />
+        <meta name="description" content="A Local and Global Culture Map" />
       </Head>
       <body>
         <Main />
